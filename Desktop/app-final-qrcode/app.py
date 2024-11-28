9101e7408a7e789980d95c0dc11a10c2a5a7f9bf
abhishek-edubull <abhishek@edubull.com>
2024-11-28 16:54:37 +0530

    Update app.py
    again trying to fix the last problem of duplicate buttons

--- Desktop/app-final-qrcode/app.py
+++ Desktop/app-final-qrcode/app.py
@@ -199,13 +199,13 @@ def main_screen():
     """, unsafe_allow_html=True)
 
 # Logic for Generate Learning Path
-        if st.button("🧠 Generate Learning Path"):
-            weak_concepts = st.session_state.auth_data.get("WeakConceptList", [])
-            if weak_concepts:
-                learning_path = generate_learning_path(weak_concepts)
-                display_learning_path(learning_path)
-            else:
-                st.error("No weak concepts found!")
+    if st.button("🧠 Generate Learning Path"):
+        weak_concepts = st.session_state.auth_data.get("WeakConceptList", [])
+        if weak_concepts:
+            learning_path = generate_learning_path(weak_concepts)
+            display_learning_path(learning_path)
+        else:
+            st.error("No weak concepts found!")
     
         
         # Display available concepts with topic name
