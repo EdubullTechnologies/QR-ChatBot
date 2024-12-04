eab9d3b62fdf0ce078ed18ad5452833c1a6cbb40
samrat090 <samrat@edubull.com>
2024-12-04 17:34:16 +0530

    Update app.py

--- Desktop/app-final-qrcode/app.py
+++ Desktop/app-final-qrcode/app.py
@@ -330,7 +330,7 @@ def main_screen():
             st.session_state.clear()  # Clears all session states
             st.rerun()  # Refresh the app to go back to the login screen
 
-    st.title(f"Hello {user_name}, 🤖 EeeBee AI buddy is here to help you with :blue[{topic_name]", anchor=None)
+    st.title(f"Hello {user_name}, 🤖 EeeBee AI buddy is here to help you with :blue[{topic_name}]", anchor=None)
 
     # Tabs for different functionalities
     tab1, tab2, tab3 = st.tabs(["Chat", "Learning Path", "Resources"])
