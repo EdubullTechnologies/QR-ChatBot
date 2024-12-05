a8b2818b12ba61a5fb1155c4f5dc21fd3b5e831a
abhishek-edubull <abhishek@edubull.com>
2024-12-05 16:49:00 +0530

    Update app.py
    testing

--- Desktop/app-final-qrcode/app.py
+++ Desktop/app-final-qrcode/app.py
@@ -80,16 +80,21 @@ hide_st_style = """
 st.markdown(hide_st_style, unsafe_allow_html=True)
 
 # Hide the profile on the bottom right
-st.markdown("""
-<style>
-div[data-testid="stToolbar"] {
-    visibility: hidden;
-    height: 0;
-    width: 0;
-    overflow: hidden;
-}
-</style>
-""", unsafe_allow_html=True)
+if 'profile_hidden' not in st.session_state:
+    st.session_state.profile_hidden = True
+    st.experimental_set_query_params(profile_hidden=st.session_state.profile_hidden)
+
+if st.session_state.get('profile_hidden', True):
+    st.markdown("""
+    <style>
+    div[data-testid="stToolbar"] {
+        visibility: hidden;
+        height: 0;
+        width: 0;
+        overflow: hidden;
+    }
+    </style>
+    """, unsafe_allow_html=True)
 
 # Hide the profile on the bottom right
 st.markdown("""
